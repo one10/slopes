5396008b8519ed011160f67cf758ae782c999e95
Ivan Balepin <csman00@gmail.com>
2022-11-01 20:38:32 -0700

    proper basic view test

--- slopes/tests.py
+++ slopes/tests.py
@@ -1,7 +1,12 @@
+import django
 from django.test import TestCase
 
+client = django.test.Client()
 
-# Create your tests here.
+
 class BasicTests(TestCase):
     def test_super_basic(self) -> None:
-        pass
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
+        response_body_str = response.content.decode("utf-8")
+        self.assertGreater(len(response_body_str), 1)
