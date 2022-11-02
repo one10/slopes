9dbbc1a3e5eda4064a1c46924a66ec753ecfb116
Ivan Balepin <csman00@gmail.com>
2022-11-01 22:53:40 -0700

    adding templates

--- slopes/urls.py
+++ slopes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("slope/<int:slope_id>", views.slope, name="get_slope"),
+    path("unused_test_view", views.unused_test_view, name="unused_test_view"),
 ]
--- slopes/views.py
+++ slopes/views.py
@@ -1,10 +1,27 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpRequest, HttpResponse
+from django.template import loader
+
+from slopes.models import SlopeUpdate
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_slope_update_list = SlopeUpdate.objects.order_by("-created_at")[:5]
+    template = loader.get_template("slopes/index.html")
+    context = {
+        "latest_slope_update_list": latest_slope_update_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def slope(request: HttpRequest, slope_id: int) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index. %s" % slope_id)
+
+
+def unused_test_view(request: HttpRequest) -> HttpResponse:
+    latest_slope_update_list = SlopeUpdate.objects.order_by("-created_at")[:5]
+    template = loader.get_template("slopes/unused_test_view.html")
+    context = {
+        "latest_slope_update_list": latest_slope_update_list,
+    }
+    return HttpResponse(template.render(context, request))
