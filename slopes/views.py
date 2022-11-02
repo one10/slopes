a51e667f4ac9226661ff8354eeb1b0811c0dd5a6
Ivan Balepin <csman00@gmail.com>
2022-11-01 23:43:25 -0700

    somewhat ready index

--- slopes/views.py
+++ slopes/views.py
@@ -1,18 +1,27 @@
-from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
-from slopes.models import SlopeUpdate
+from slopes.models import SlopeUpdate, Slope
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    latest_slope_update_list = SlopeUpdate.objects.order_by("-created_at")[:5]
-    template = loader.get_template("slopes/index.html")
+    slope_list = Slope.objects.order_by("name")
+    latest_updates_for_each_slope = []
+    for slope_item in slope_list:
+        latest_update = SlopeUpdate.objects.filter(slope=slope_item).latest()
+        latest_update_dict = {}
+        latest_update_dict["slope"] = latest_update.slope
+        latest_update_dict["type"] = latest_update.type.replace('_', ' ').title()
+        latest_update_dict["effective_date"] = latest_update.effective_date
+        latest_update_dict["status"] = latest_update.status.replace('_', '').title()
+        latest_update_dict["created_at"] = latest_update.created_at
+        latest_updates_for_each_slope.append(latest_update_dict)
     context = {
-        "latest_slope_update_list": latest_slope_update_list,
+        "slope_list": slope_list,
+        "latest_updates_for_each_slope": latest_updates_for_each_slope,
     }
-    return HttpResponse(template.render(context, request))
-
+    return render(request, 'slopes/index.html', context)
 
 def slope(request: HttpRequest, slope_id: int) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index. %s" % slope_id)
