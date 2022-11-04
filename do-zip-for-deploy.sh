35765a2e9208a90d4b5cf2c887d204d1c68bf3b7
Ivan Balepin <csman00@gmail.com>
2022-11-04 00:20:21 -0700

    trying-to-get-static-to-work

--- do-zip-for-deploy.sh
+++ do-zip-for-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm *zip
 temp_dir=`mktemp -d`
 
 mv .git .gitignore .idea db00.sqlite3 venv $temp_dir
@@ -16,12 +17,3 @@ mv $temp_dir/venv .
 
 rmdir $temp_dir
 
-#i-rw-r--r--@  1 macuser  staff    6148 Nov  2 02:52 .DS_Store
-#drwxr-xr-x  15 macuser  staff     480 Nov  3 23:04 .git
-#-rw-r--r--   1 macuser  staff    1839 Nov  1 22:27 .gitignore
-#drwxr-xr-x   8 macuser  staff     256 Nov  2 02:34 .idea
-#drwxr-xr-x   5 macuser  staff     160 Nov  1 19:55 .mypy_cache
-#-rw-r--r--@  1 macuser  staff     452 Nov  1 21:43 README.md
-#-rw-r--r--   1 macuser  staff  143360 Nov  1 22:21 db00.sqlite3
-#-rw-r--r--   1 macuser  staff  147456 Nov  2 01:49 db_local.sqlite3
-#drwxr-xr-x   6 macuser  staff     192 Nov  1 19:35 venv
