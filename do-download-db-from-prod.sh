ec40a9951bac182fcdbd6005d5ef6a6c7f00c23c
Ivan Balepin <csman00@gmail.com>
2022-11-08 00:25:49 -0800

    fix

--- do-download-db-from-prod.sh
+++ do-download-db-from-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -i ~/.ssh/oregon-2022-00.pem ec2-user@100.20.90.102:/var/app/current/db.sqlite3 .
+scp -i ~/.ssh/oregon-2022-00.pem ec2-user@100.20.90.102:/var/app/current/db.sqlite3 ./db.sqlite3_prod
